364cd72b9d65174a92c8f7b2062b2a1ee6d5029f
cemile-nur <78272058+cemile-nur@users.noreply.github.com>
2022-05-01 00:12:08 +0300

    Recyclerview Reverse

--- app/src/main/java/com/pakt_games/mydictionary/repo/MyDictionaryRepository.kt
+++ app/src/main/java/com/pakt_games/mydictionary/repo/MyDictionaryRepository.kt
@@ -2,6 +2,7 @@ package com.pakt_games.mydictionary.repo
 
 import com.pakt_games.mydictionary.db.DictionaryWords
 import com.pakt_games.mydictionary.db.DictionaryWordsDao
+import java.util.Collections.*
 
 class MyDictionaryRepository(private val dictionaryWordsDao: DictionaryWordsDao) {
 
@@ -9,6 +10,7 @@ class MyDictionaryRepository(private val dictionaryWordsDao: DictionaryWordsDao)
 
     suspend fun getAllSavedWordsData(): List<DictionaryWords> {
         val readAllWords: List<DictionaryWords> = dictionaryWordsDao.getAll()
+        reverse(readAllWords)
         return readAllWords
     }
 }
